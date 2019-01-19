1932ff7eb6f42cf872b21334b0a5659b07c4fd9b
Andrés Muñoz <andru255@gmail.com>
2019-01-19 11:29:47 -0500

    feat(home): Plantilla de landing y formulario

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,71 @@
 <template>
   <div id="app">
-    <Form />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="py-3">
+      <h1 class="text-center">XChangex</h1>
+    </header>
+    <div class="nav-scroller py-1 mb-2">
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Características</a>
+        <a class="p-2 text-dark" href="#">Empresas</a>
+        <a class="p-2 text-dark" href="#">Soporte</a>
+        <a class="p-2 text-dark" href="#">Paquetes</a>
+      </nav>
+    </div>
+    <div class="container-form">
+      <div class="container">
+          <Form />
+      </div>
+    </div>
+    <footer class="container">
+        <div class="pt-4 my-md-5 pt-md-5 border-top">
+          <div class="row">
+            <div class="col-6 col-md">
+              <h5>Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Cool stuff</a></li>
+                <li><a class="text-muted" href="#">Random feature</a></li>
+                <li><a class="text-muted" href="#">Team feature</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Resource</a></li>
+                <li><a class="text-muted" href="#">Resource name</a></li>
+                <li><a class="text-muted" href="#">Another resource</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>About</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Team</a></li>
+                <li><a class="text-muted" href="#">Privacy</a></li>
+                <li><a class="text-muted" href="#">Terms</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <p>&copy; 2019 XChangex, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// components
 import Form from "./components/Form.vue";
+import "bootstrap";
 
 export default {
   name: "app",
   components: {
-    HelloWorld,
     Form
   }
 };
 </script>
+
+<style lang="css">
+  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+</style>
 
 <style>
 #app {
@@ -26,6 +74,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container-form {
+  padding: 4rem 2rem;
+  background-color: #e9ecef;
+}
+footer {
+  text-align: left;
 }
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,16 @@
 <template>
-  <form>
-    <p>HI I'm the form</p>
+  <form class="py-3" v-on:submit.prevent>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <label for="dollarAmount">Monto en dólares</label>
+        <input class="form-control" v-model="price" placeholder="USD">
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="euroAmount">Monto en euros</label>
+        <input class="form-control" v-model="change" placeholder="EU">
+      </div>
+    </div>
+    <button class="btn btn-primary btn-lg" v-on:click="calculate">Calcular</button>
   </form>
 </template>
 
@@ -8,8 +18,22 @@
 <script>
 export default {
   name: "Form",
-  props: {
-    entries: [{ content: "foo" }]
+  data: function() {
+    return {
+      price: undefined,
+      change: undefined
+    };
+  },
+  methods: {
+    calculate: function() {
+      console.log("do request!!");
+    }
   }
 };
 </script>
+
+<style>
+input {
+  text-align: center;
+}
+</style>
